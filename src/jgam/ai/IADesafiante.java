92cc2af7877e78be1fd37732d81187168650ab3b
Alpaca-kun <ra90558@uem.br>
2017-05-28 16:01:55 -0300

    adicao de gitignore e modificacao no IAdesafiante

--- src/jgam/ai/IADesafiante.java
+++ src/jgam/ai/IADesafiante.java
@@ -5,13 +5,14 @@
  */
 package jgam.ai;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import jgam.game.BoardSetup;
+import jgam.game.PossibleMoves;
 import jgam.game.SingleMove;
 
-/**
- *
- * @author rafael
- */
 public class IADesafiante implements AI{
         /**
      * initialize this instance. Is called before it is used to
@@ -60,9 +61,70 @@ public class IADesafiante implements AI{
      * @return SingleMove[] a complete set of moves.
      * @throws CannotDecideException if the AI cannot decide which moves to make
      */
+
+    double
+
+    double agressiveHeuristic (BoardSetup boardSetup) {
+      double returnValue = 0.0;
+      int player = boardSetup.getPlayerAtMove();
+      int opponent = 3 - player;
+
+      for (int i = 1; i <= 24; i++) {
+        int checkersPoint = boardSetup.getPoint(player, i);
+
+        //Casas com mais de 3 pecas acumuladas eh ruim, pois o agressivo pretende mover o rapido possivel
+        if (checkersPoint >= 3) {
+          returnValue -= 75.0;
+        }
+
+        else if (checkersPoint == 1){
+          returnValue -= 25.0;
+        }
+      }
+
+      //Para o agressivo, quanto mais peca inimiga no inicio, eh melhor
+      if (boardSetup.getPoint(opponent, 0) >= 1)
+        returnValue += 100.0;
+
+      if (boardSetup.getPoint(player, 0) >= 1) 
+        returnValue -= 50.0;
+
+      int point = boardSetup.getPoint(player, 25);
+      returnValue += checkersPoint * 5.0; 
+
+      point = boardSetup.getPoint(opponent, 25);
+      returnValue -= checkersPoint * 10.0;
+
+      return returnValue;
+    }
+
     @Override
     public SingleMove[] makeMoves(BoardSetup boardSetup) throws CannotDecideException {
-        return new SingleMove[0];
+        double bestValue = Double.NEGATIVE_INFINITY;
+        int bestIndex = -1;
+
+        PossibleMoves pm = new PossibleMoves(boardSetup);
+        List list = pm.getPossbibleNextSetups();
+
+        int index = 0;
+        for (Iterator iter = list.iterator(); iter.hasNext(); index++) {
+            BoardSetup setup = (BoardSetup) iter.next();
+            double value = eval(setup);
+
+            if (value > bestValue) {
+                bestValue = value;
+                bestIndex = index;
+            }
+        }
+
+        if (bestIndex == -1)
+            return new SingleMove[0];
+        else {
+            // Sy stem.out.println("Evaluation for this move: "+bestValue);
+            return pm.getMoveChain(bestIndex);
+        }
+
+        //return new SingleMove[0];
     }
 
     /**
